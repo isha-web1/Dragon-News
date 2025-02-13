bfd651a03c91a1a37351899d2a11c2a467ffef21
isha-web1 <isha.web1@gmail.com>
2025-02-14 01:56:08 +0600

    category wise data load

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -4,6 +4,7 @@ import LatestNews from '../components/LatestNews';
 import Navbar from '../components/Navbar';
 import LeftNavbar from '../components/LeftNavbar';
 import RightNavbar from '../components/RightNavbar';
+import { Outlet } from 'react-router-dom';
 
 const HomeLayout = () => {
     return (
@@ -17,7 +18,7 @@ const HomeLayout = () => {
            </section>
            <main className='w-5/6 mx-auto py-5 grid md:grid-cols-12 gap-3'>
             <aside className='col-span-3'><LeftNavbar/></aside>
-            <aside className='col-span-6'>main content</aside>
+            <aside className='col-span-6'><Outlet/></aside>
             <aside className='col-span-3'><RightNavbar/></aside>
            </main>
         </div>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,24 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import HomeLayout from "../layout/HomeLayout";
+import CategoryNews from "../pages/CategoryNews";
 
 export const router = createBrowserRouter([
-    {
-        path : '/',
-        element : <HomeLayout/>
-    }
-])
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        path: "",
+        element: <Navigate to={"/category/01"} />,
+      },
+      {
+        path: "/category/:id",
+        element: <CategoryNews />,
+        loader: ({ params }) =>
+          fetch(
+            `https://openapi.programming-hero.com/api/news/category/${params.id}`
+          ),
+      },
+    ],
+  },
+]);
