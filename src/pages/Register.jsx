35ef1c6d7747485ce04aeaae5bd35b293658c6e3
isha-web1 <isha.web1@gmail.com>
2025-02-15 15:08:43 +0600

    implement contextAPI & register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,29 +1,57 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
 
 const Register = () => {
+  const {createNewUser,  setUser} = useContext(AuthContext)
+  
+  const handleSubmit = (e) =>{
+       e.preventDefault();
+      //  get data 
+      const form = new FormData(e.target);
+      const name = form.get('name');
+      const email = form.get('email');
+      // const photo = form.get('photo');
+      const password = form.get('password');
+      
+      createNewUser(email,password)
+      .then(result=>{
+        // console.log("Result from createNewUser:", result);
+       const user = result?.user;
+       setUser(user)
+       console.log(user)
+      })
+      .catch((error) =>{
+        const errorCode = error.code;
+        const errorMessage = error.message
+        console.log(errorCode, errorMessage)
+      })
+      
+  
+      
+  }
   return (
     <div className="min-h-screen flex justify-center items-center">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
         <h1 className="text-2xl font-semibold text-center py-2">
           Register  your Account
         </h1>
-        <div className="card-body">
+        <form onSubmit={handleSubmit} className="card-body">
           <fieldset className="fieldset">
             <label className="fieldset-label">Name</label>
-            <input type="text" className="input" placeholder="Name" />
+            <input name="name" type="text" className="input" placeholder="Name" />
             <label className="fieldset-label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
+            <input name="email" type="email" className="input" placeholder="Email" />
             <label className="fieldset-label">Photo URL</label>
-            <input type="text" className="input" placeholder="photo_url" />
+            <input name="photo" type="text" className="input" placeholder="photo_url" />
             <label className="fieldset-label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
+            <input name="password" type="password" className="input" placeholder="Password" />
             <div>
               <a className="link link-hover">Forgot password?</a>
             </div>
             <button className="btn btn-neutral mt-4">Register</button>
           </fieldset>
-        </div>
+        </form>
         <p className="text-center font-semibold pb-3 ">
           Already have an account ?{" "}
           <Link to="/auth/login" className="underline text-blue-400">
